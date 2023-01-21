01fe982e525a3be0cf9a1815b403358009705560
IdkwhatImD0ing <91146598+IdkwhatImD0ing@users.noreply.github.com>
2023-01-21 14:54:58 -0800

    Firebase helpers

--- auth0/src/components/Profile.js
+++ auth0/src/components/Profile.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
+import {useAuth0} from '@auth0/auth0-react';
 import JSONPretty from 'react-json-pretty';
 import Avatar from '@mui/material/Avatar';
 
 const Profile = () => {
-    const { user, isAuthenticated } = useAuth0();
-    return (
-        isAuthenticated && (
-            <div className='profile'>
-                <img src={user.picture} alt={user.name}/>
-                <h2>Welcome back, {user.name}!</h2>
-                <p>Logged in as {user.email}</p>
-                {/* <JSONPretty data={user} /> */}
-                {/* {JSON.stringify(user, null, 2)} */}
-            </div>
-        )
-        
+  const {user, isAuthenticated} = useAuth0();
+  console.log(user);
+  return (
+    isAuthenticated && (
+      <div className="profile">
+        <img src={user.picture} alt={user.name} />
+        <h2>Welcome back, {user.name}!</h2>
+        <p>Logged in as {user.email}</p>
+        {/* <JSONPretty data={user} /> */}
+        {/* {JSON.stringify(user, null, 2)} */}
+      </div>
     )
-}
+  );
+};
 
-export default Profile
+export default Profile;
