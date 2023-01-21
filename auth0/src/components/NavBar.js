17a3da5aab9aa02e6f9fe1039e652f5f9b710bd2
coderlore <coderlore@gmail.com>
2023-01-21 00:35:15 -1000

    Added frontend with navbar and drawer

---
+++ auth0/src/components/NavBar.js
@@ -0,0 +1,156 @@
+// import { AppBar, IconButton, Toolbar, Typography, Stack, Button } from '@mui/material'
+import LocalActivityIcon from '@mui/icons-material/LocalActivity';
+import { useAuth0 } from '@auth0/auth0-react';
+import LogoutButton from './LogoutButton';
+import Profile from './Profile';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import AddIcon from '@mui/icons-material/Add';
+import LogoutIcon from '@mui/icons-material/Logout';
+
+const drawerWidth = 240;
+
+const NavBar = (props) => {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const { user, isAuthenticated } = useAuth0();
+
+  const drawer = (
+    <div>
+      <Toolbar />
+      <Divider />
+        <List>
+            <ListItem key='my-events' disablePadding>
+                <ListItemButton>
+                <ListItemIcon>
+                <LocalActivityIcon />
+                </ListItemIcon>
+                <ListItemText primary='My events' />
+                </ListItemButton>
+            </ListItem>
+            <ListItem key='add-event' disablePadding>
+                <ListItemButton>
+                <ListItemIcon>
+                <AddIcon />
+                </ListItemIcon>
+                <ListItemText primary='Add new event' />
+                </ListItemButton>
+            </ListItem>
+        </List>
+      <Divider />
+      <List>
+          <ListItem key='logout' disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+              <LogoutIcon />
+              </ListItemIcon>
+              <ListItemText primary='Log Out' />
+            </ListItemButton>
+          </ListItem>
+      </List>
+    </div>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Event Finder
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: 'none', sm: 'block' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+      >
+        <Toolbar />
+        <Profile />
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
+          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
+          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
+          Convallis convallis tellus id interdum velit laoreet id donec ultrices.
+          Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
+          nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
+          leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
+          feugiat vivamus at augue. At augue eget arcu dictum varius duis at
+          consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
+          sapien faucibus et molestie ac.
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
+
+export default NavBar
--- auth0/src/components/Profile.js
+++ auth0/src/components/Profile.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import JSONPretty from 'react-json-pretty';
-
+import Avatar from '@mui/material/Avatar';
 
 const Profile = () => {
     const { user, isAuthenticated } = useAuth0();
     return (
         isAuthenticated && (
-            <div>
+            <div className='profile'>
                 <img src={user.picture} alt={user.name}/>
-                <h2>{user.name}</h2>
-                <p>{user.email}</p>
-                <JSONPretty data={user} />
+                <h2>Welcome back, {user.name}!</h2>
+                <p>Logged in as {user.email}</p>
+                {/* <JSONPretty data={user} /> */}
                 {/* {JSON.stringify(user, null, 2)} */}
             </div>
         )
