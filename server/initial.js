b00d719ae987c09f75daa631737ff9c15542c202
IdkwhatImD0ing <91146598+IdkwhatImD0ing@users.noreply.github.com>
2023-01-21 13:02:35 -0800

    added basic routes

--- server/initial.js
+++ server/initial.js
@@ -1,10 +1,13 @@
-const createInitialState = (name, date, location) => {
+const createInitialState = (name, date, location, user) => {
   const obj = {
     name: name,
     date: date,
     location: location,
     registered: {},
+    creator: user,
   };
 
   return obj;
 };
+
+module.exports = createInitialState;
